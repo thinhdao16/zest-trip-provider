3e200400ead69a886d6ca0a899ca2f096aacf167
thinhdao16 <daothinh1105@gmail.com>
2023-11-01 22:04:54 +0700

    es 3

--- src/components/Layout/AuthLayout.tsx
+++ src/components/Layout/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { sidebarToggle } from "../../utils/toggler";
 import BottomNavbar from "../BottomNavbar/Index";
-import Sidebar from "../Sidebar/Sidebar.tsx";
+import Sidebar from "../../../src/components/sidebar/Sidebar";
 
 function AuthLayout(): JSX.Element {
   const isDesktop = (): boolean => document.body.clientWidth > 768;
