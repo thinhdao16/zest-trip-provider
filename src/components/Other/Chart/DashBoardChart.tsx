5967415f1cb45de6fd1358218b97f07b3b3e6838
thynhacute <nhathy07@gmail.com>
2023-11-23 04:13:03 +0700

    addd chart

---
+++ src/components/Other/Chart/DashBoardChart.tsx
@@ -0,0 +1,166 @@
+import dayjs from "dayjs";
+import AppWebsiteVisits from "./app-website-visits";
+import { useSelector } from "react-redux";
+
+interface Booking {
+  updated_at: string;
+  paid_price: string;
+  original_price: string;
+  refund_ammount: string;
+  [key: string]: string | undefined; // Index signature
+  // Add other properties as needed
+}
+
+function DashboardChart() {
+  const { booking } = useSelector((state: any) => state.booking);
+
+  const labelWeeks = Array.from({ length: 7 }, (_, i) =>
+    dayjs().subtract(i, "day").format("ddd")
+  ).reverse();
+
+  function calculateTotalByDay(
+    bookings: Booking[],
+    targetDay: string,
+    propertyName: string,
+    field: string,
+    startDate: string,
+    endDate: string
+  ) {
+    const totalByDay: Record<string, number> = {};
+    const totalByMonth: Record<string, number> = {};
+    const totalByWeek: Record<string, number> = {};
+    if (field === "chart_day") {
+      const recentBookings = bookings?.filter((booking) => {
+        const bookedDates = dayjs(booking.updated_at);
+        const today = dayjs();
+        const diffInDays = today.diff(bookedDates, "day");
+        return diffInDays >= 0 && diffInDays < 7;
+      });
+      recentBookings?.forEach((booking) => {
+        const dayOfWeek = dayjs(booking.updated_at).format("ddd");
+        const propertyValue = parseInt(booking[propertyName] || "0");
+
+        if (dayOfWeek === targetDay) {
+          totalByDay[targetDay] = (totalByDay[targetDay] || 0) + propertyValue;
+        }
+      });
+
+      return totalByDay[targetDay] || 0;
+    }
+
+    if (field === "chart_month") {
+      const recentBookings = bookings?.filter((booking) => {
+        const bookedDates = dayjs(booking.updated_at);
+        const tomonth = dayjs();
+        const diffInMonths = tomonth.diff(bookedDates, "month");
+        return diffInMonths >= 0 && diffInMonths < 3;
+      });
+
+      recentBookings?.forEach((booking) => {
+        const monthOfYear = dayjs(booking.updated_at).format("MMM");
+        const propertyValue = parseInt(booking[propertyName] || "0");
+
+        if (monthOfYear === targetDay) {
+          totalByMonth[targetDay] =
+            (totalByMonth[targetDay] || 0) + propertyValue;
+        }
+      });
+
+      return totalByMonth[targetDay] || 0;
+    }
+    if (field === "chart_week") {
+      const recentBookings = bookings?.filter((booking) => {
+        const bookedDate = dayjs(booking.updated_at);
+        const startDateObj = dayjs(startDate).subtract(1, "day");
+        const endDateObj = dayjs(endDate).add(1, "day");
+        return (
+          bookedDate.isAfter(startDateObj, "day") &&
+          bookedDate.isBefore(endDateObj, "day")
+        );
+      });
+      recentBookings?.forEach((booking) => {
+        const dayOfMonth = dayjs(booking.updated_at).format("D");
+        const propertyValue = parseInt(booking[propertyName] || "0");
+        totalByWeek[dayOfMonth] =
+          (totalByWeek[dayOfMonth] || 0) + propertyValue;
+      });
+
+      const total = Object.values(totalByWeek).reduce(
+        (acc, value) => acc + value,
+        0
+      );
+
+      return total;
+    }
+
+    return 0;
+  }
+  return (
+    <>
+      <main className="h-full">
+        {/* Main Content */}
+        <div className="text-xl font-medium check mb-4">Revenue analytics</div>
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-9">
+            <AppWebsiteVisits
+              title="Weekly Activity"
+              chart={{
+                labels: labelWeeks,
+                series: [
+                  // {
+                  //   name: "Paid price",
+                  //   type: "column",
+                  //   fill: "solid",
+                  //   data: labelWeeks.map((day) =>
+                  //     calculateTotalByDay(
+                  //       booking,
+                  //       day,
+                  //       "paid_price",
+                  //       "chart_day",
+                  //       "0",
+                  //       "0"
+                  //     )
+                  //   ),
+                  // },
+                  {
+                    name: "Paid price",
+                    type: "area",
+                    fill: "gradient",
+                    data: labelWeeks.map((day) =>
+                      calculateTotalByDay(
+                        booking,
+                        day,
+                        "paid_price",
+                        "chart_day",
+                        "0",
+                        "0"
+                      )
+                    ),
+                  },
+                  // {
+                  //   name: "Refund amount",
+                  //   type: "line",
+                  //   fill: "solid",
+                  //   data: labelWeeks.map((day) =>
+                  //     calculateTotalByDay(
+                  //       booking,
+                  //       day,
+                  //       "refund_ammount",
+                  //       "chart_day",
+                  //       "0",
+                  //       "0"
+                  //     )
+                  //   ),
+                  // },
+                ],
+              }}
+            />
+          </div>
+          <div className="col-span-3">abc</div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default DashboardChart;
---
+++ src/components/Other/Chart/app-website-visits.tsx
@@ -0,0 +1,81 @@
+import PropTypes from "prop-types";
+
+import Box from "@mui/material/Box";
+import Chart from "./chart";
+import { useChart } from ".";
+
+// ----------------------------------------------------------------------
+
+export default function AppWebsiteVisits({
+  title,
+  chart,
+}: {
+  title: string; // Specify the type for 'title'
+  chart: any; // You might want to replace 'any' with a more specific type for your chart data
+  // ... (add types for other props if needed)
+}) {
+  const { labels, colors, series, options } = chart;
+
+  // Tạo bản sao của mảng series để không ảnh hưởng đến dữ liệu gốc
+  const formattedSeries = series.map(
+    (dataSet: { data: { value: string }[] }) => {
+      const formattedData = dataSet.data.map((value) => value);
+      return {
+        ...dataSet,
+        data: formattedData,
+      };
+    }
+  );
+
+  console.log(parseInt(formattedSeries[0]?.data[6], 10));
+  const chartOptions = useChart({
+    colors,
+    plotOptions: {
+      bar: {
+        columnWidth: "16%",
+      },
+    },
+    fill: {
+      type: formattedSeries.map((i: any) => i.fill),
+    },
+    labels,
+    xaxis: {
+      type: "category",
+    },
+    tooltip: {
+      shared: true,
+      intersect: false,
+      y: {
+        formatter: (value: string) => {
+          if (typeof value !== "undefined") {
+            return value;
+          }
+          return value;
+        },
+      },
+    },
+    ...options,
+  });
+
+  return (
+    <div className="bg-white p-4 shadow-custom-card-mui rounded-xl">
+      <span className="font-medium">{title}</span>
+      <Box>
+        <Chart
+          dir="ltr"
+          type="line"
+          series={formattedSeries}
+          options={chartOptions}
+          width="100%"
+          height={200}
+        />
+      </Box>
+    </div>
+  );
+}
+
+AppWebsiteVisits.propTypes = {
+  chart: PropTypes.object,
+  subheader: PropTypes.string,
+  title: PropTypes.string,
+};
