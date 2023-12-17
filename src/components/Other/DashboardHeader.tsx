871ac7430b40f2b142d99564d5e7ac49a978f145
thinhdao16 <daothinh1105@gmail.com>
2023-12-17 18:51:19 +0700

    delete unuse

--- src/components/Other/DashboardHeader.tsx
+++ src/components/Other/DashboardHeader.tsx
@@ -1,5 +1,4 @@
-import { Link } from "react-router-dom";
-import { faBars, faBell } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSelector } from "react-redux";
 import SliceEmailToName from "../../utils/SliceEmailToName";
